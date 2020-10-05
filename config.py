5f602c79cb3c963e9d893265047b8babdb7e01a3
lazyKT <kyaw.thitlwin.me@gmail.com>
2020-10-05 21:19:58 +0800

    Resolved Conflicts

--- config.py
+++ config.py
@@ -2,11 +2,8 @@ import os
 
 
 DEBUG = False
-<<<<<<< HEAD
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///data.db")
 HOST_EMAIL = "kyaw.thitlwin.me@gmail.com"
 FORWARD_EMAIL = "kyaw.thitlwin.dev@gmail.com"
-=======
-CELERY_TASKMETA_URL = 'postgres://kt:Kyawthit18$@localhost:5432/kt'
+CELERY_TASKMETA_URL = 'sqlite:///data.db'
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///data.db")
->>>>>>> 3a3c7014afcedfa63eadfa26a80a6bc4697c67fc
